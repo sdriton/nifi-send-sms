59b0b7d64578474b5d56545e755ec721f32ee0eb
Driton Salihu <driton.geci@gmail.com>
2024-11-18 22:45:55 -0500

    Fine tune rate limiting and test cases.

--- nifi-send-sms-processors/src/test/java/com/driton/nifi/sms/PutSmsTest.java
+++ nifi-send-sms-processors/src/test/java/com/driton/nifi/sms/PutSmsTest.java
@@ -17,11 +17,18 @@
 package com.driton.nifi.sms;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.nifi.processor.Relationship;
 import org.apache.nifi.util.MockFlowFile;
@@ -32,6 +39,12 @@ import org.junit.jupiter.api.Test;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvEntry;
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sns.SnsClient;
 
 class PutSmsTest {
 
@@ -126,5 +139,70 @@ class PutSmsTest {
         String outputFlowfileContent = new String(testRunner.getContentAsByteArray(outputFlowfile));
         System.out.println("Output flowfile content: " + outputFlowfileContent);
         assertEquals(flowFileContent, outputFlowfileContent);
+    }
+
+    // @Test
+    void testRateLimiter(){
+        PutSms processor = (PutSms) testRunner.getProcessor();
+        Dotenv dotEnv = null; 
+        try { 
+            dotEnv = Dotenv.configure().ignoreIfMissing().systemProperties().load();
+        } catch(Exception e){
+            System.out.println("DotEnv file is missing."); 
+            return;
+        }
+
+        String awsAccessKey = dotEnv.get("AWS_ACCESS_KEY");
+        String awsAccessSecret = dotEnv.get("AWS_ACCESS_SECRET");
+        
+
+        SnsClient snsClient = SnsClient.builder().region(Region.of("us-east-1"))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(awsAccessKey, awsAccessSecret)))
+                .build();
+        List<String> phoneNumbers = new ArrayList<>();
+        for(int i = 0; i < 50; ++i){
+            phoneNumbers.add("+111133344" + String.format("%02d", i));
+        }
+
+        RateLimiter limiter = null;
+        // The limit of requests to AWS SNS Publish SMS is 20 requests per second.
+        int limitForPeriod = 18;
+        // The timeout between batches of 20 requests. It should be 1 second as per AWS Limits.
+        long timeoutDuration = 1500;
+        // The limit of refresh period 
+        long limitRefreshPeriod = 1500;
+        
+        RateLimiterConfig config = RateLimiterConfig.custom()
+                .limitForPeriod(limitForPeriod)
+                .limitRefreshPeriod(Duration.ofMillis(limitRefreshPeriod))
+                .timeoutDuration(Duration.ofMillis(timeoutDuration))
+                .build();
+
+        limiter = RateLimiter.of("awssnscalllimiter_rps", config);
+        List<String> listOfTimestamps = new ArrayList<>();
+        String message = null;
+        for (String phoneNumber : phoneNumbers) {
+            try {
+                message = processor.doSendSms(snsClient, limiter, phoneNumber, "Test rate limiter.");
+                System.out.println(new Date().toString());
+                Calendar rightNow = Calendar.getInstance();
+                int second = rightNow.get(Calendar.SECOND);
+                int minute = rightNow.get(Calendar.MINUTE);
+                String stamp = String.format("%s",   minute + second + "");
+                listOfTimestamps.add(stamp);
+                assertNotNull(message);
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        Map<String, Long> counts = listOfTimestamps.stream()
+            .collect(Collectors.groupingBy(value -> value, Collectors.counting()));
+
+        // Print the result
+        counts.forEach((key, count) -> { 
+            System.out.println(key + ": " + count);
+            assertTrue(count <= 20);
+        });
     }
 }
